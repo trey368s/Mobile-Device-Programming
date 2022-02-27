430cd7220cf21a129ab665eeec16c2a930e4161c
trey368s <84042739+trey368s@users.noreply.github.com>
2022-02-26 22:04:55 -0500

    Menu Dropdown

--- app/src/main/java/com/mobiledevelopmentfoodapp/MainActivity.kt
+++ app/src/main/java/com/mobiledevelopmentfoodapp/MainActivity.kt
@@ -6,12 +6,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,7 +29,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FoodAppTheme {
-                Box (modifier = Modifier .fillMaxSize() .background(Color.LightGray)) {
+                Box (modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.LightGray)) {
                     // A surface container using the 'background' color from the theme
                     Surface(
                         modifier = Modifier.fillMaxWidth(),
@@ -38,6 +40,7 @@ class MainActivity : ComponentActivity() {
                         Header("Android")
                     }
                     ButtonBar()
+                    Menu()
                 }
             }
         }
@@ -63,12 +66,45 @@ fun ButtonBar() {
             onClick = {
                 Toast.makeText(context, "TAKES USER TO CART PAGE", Toast.LENGTH_LONG).show()
             },
-            modifier = Modifier.padding(all = 50.dp),
+            modifier = Modifier.padding(all = 55.dp),
             enabled = true,
             border = BorderStroke(width = 1.dp, brush = SolidColor(Color.Blue)),
             shape = MaterialTheme.shapes.medium,
         ) {
             Text(text = "View Cart", color = Color.White)
+        }
+    }
+}
+
+@Composable
+fun Menu() {
+    val items = listOf("A", "B", "C", "D", "E", "F")
+    var countryName : String by remember { mutableStateOf("Menu Categories") }
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+        Row(
+            Modifier
+                .padding(100.dp)
+                .clickable {
+                    expanded = !expanded
+                }
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text= countryName, fontSize = 18.sp, modifier = Modifier.padding(end = 8.dp))
+            Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = "")
+            DropdownMenu(expanded = expanded, onDismissRequest = {expanded = false}) {
+                items.forEachIndexed {
+                        index, s -> DropdownMenuItem(onClick = {
+                    expanded = false
+
+                }) {
+                    Text(text = "Food Category")
+                }
+                }
+            }
         }
     }
 }
