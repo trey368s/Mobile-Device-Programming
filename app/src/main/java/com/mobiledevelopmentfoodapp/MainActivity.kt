3c40bd875d5a9c856b25ef79e3e2b520a7804db3
trey368s <84042739+trey368s@users.noreply.github.com>
2022-02-26 21:04:23 -0500

    Added Header

--- app/src/main/java/com/mobiledevelopmentfoodapp/MainActivity.kt
+++ app/src/main/java/com/mobiledevelopmentfoodapp/MainActivity.kt
@@ -3,13 +3,19 @@ package com.mobiledevelopmentfoodapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.mobiledevelopmentfoodapp.ui.theme.FoodAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +23,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FoodAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Box (modifier = Modifier .fillMaxSize() .background(Color.LightGray)) {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxWidth(),
+                        color = MaterialTheme.colors.background
+                    ) {
+                        Header("Android")
+                    }
                 }
             }
         }
@@ -30,14 +38,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Header(name: String) {
+    Text(text = "Fastfood Restaurant", fontSize = 30.sp, textAlign = TextAlign.Center)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FoodAppTheme {
-        Greeting("Android")
+        Header("Android")
     }
 }
