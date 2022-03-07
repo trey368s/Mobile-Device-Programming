749f4094290794323ecd1bf3ce81185550a551e9
DeepFriedTwinkie123 <eobrien185@gmail.com>
2022-03-06 21:42:36 -0500

    Changes to formatting and breaks in classes to more accurately reflect kotlin conventions

--- app/src/main/java/com/mobiledevelopmentfoodapp/dto/Customer.kt
+++ app/src/main/java/com/mobiledevelopmentfoodapp/dto/Customer.kt
@@ -4,8 +4,10 @@ package com.mobiledevelopmentfoodapp.dto
 //import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-data class Customer(var name: String, var phone: String, @SerializedName("id") var customerId:Int = 0 ) {
-    override fun toString(): String {
-        return name
-    }
+data class Customer(
+    var name: String,
+    var phone: String,
+    @SerializedName("id") var customerId:Int = 0)
+{
+    override fun toString(): String { return name }
 }
--- app/src/main/java/com/mobiledevelopmentfoodapp/dto/Order.kt
+++ app/src/main/java/com/mobiledevelopmentfoodapp/dto/Order.kt
@@ -4,8 +4,12 @@ package com.mobiledevelopmentfoodapp.dto
 //import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-data class Order(var customerId: Int, var name: String, var products: ArrayList<Food>, var totalPrice: Double, @SerializedName("id") var orderId:Int = 0 ) {
-    override fun toString(): String {
-        return name
-    }
+data class Order(
+    var customerId: Int,
+    var name: String,
+    var products: ArrayList<Food>,
+    var totalPrice: Double,
+    @SerializedName("id") var orderId:Int = 0 )
+{
+    override fun toString(): String { return name }
 }
