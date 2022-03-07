466f20ca778d45972477afe8b2f017d66b263b75
abbottmh <97859319+abbottmh@users.noreply.github.com>
2022-03-07 00:16:42 -0500

    My Code Review Suggestions

--- app/src/main/java/com/mobiledevelopmentfoodapp/dto/Order.kt
+++ app/src/main/java/com/mobiledevelopmentfoodapp/dto/Order.kt
@@ -4,7 +4,13 @@ package com.mobiledevelopmentfoodapp.dto
 //import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-data class Order(var customerId: Int, var name: String, var products: ArrayList<Food>, var totalPrice: Double, @SerializedName("id") var orderId:Int = 0 ) {
+data class Order(
+    var customerId: Int,
+    var name: String,
+    var products: ArrayList<Food>,
+    var totalPrice: Double,
+    @SerializedName("id") var orderId:Int = 0 ,
+) {
     override fun toString(): String {
         return name
     }
