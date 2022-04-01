f1cfd3d049b90411a815ce3cfd980a6401148ce9
haseebikram <ikramhu@mail.uc.edu>
2022-04-01 19:14:00 -0400

    Update Type.kt

--- app/src/main/java/com/mobiledevelopmentfoodapp/ui/theme/Type.kt
+++ app/src/main/java/com/mobiledevelopmentfoodapp/ui/theme/Type.kt
@@ -11,18 +11,18 @@ val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 24.sp
     )
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontSize = 22.sp
     ),
     caption = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 20.sp
     )
     */
 )
