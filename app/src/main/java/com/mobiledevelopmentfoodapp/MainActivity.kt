209abc61a0126a659dbd9572976bbf59e6e2de0e
Jubn-yan <jubn@mail.uc.edu>
2022-04-02 21:17:49 +0800

    Add viewmodel in main activity

--- app/src/main/java/com/mobiledevelopmentfoodapp/MainActivity.kt
+++ app/src/main/java/com/mobiledevelopmentfoodapp/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,15 +20,20 @@ import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mobiledevelopmentfoodapp.ui.theme.FoodAppTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel : MainViewModel by viewModel<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            viewModel.fetchRestaurants()
+            val restaurants by viewModel.restaurant.observeAsState(initial= emptyList())
             FoodAppTheme {
                 Box (modifier = Modifier
                     .fillMaxSize()
