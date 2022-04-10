9767c696c7a7919725e5254d30f184c19924b297
trey368s <84042739+trey368s@users.noreply.github.com>
2022-04-09 20:27:09 -0400

    Fetch data from Firestore

--- app/src/main/java/com/mobiledevelopmentfoodapp/MainViewModel.kt
+++ app/src/main/java/com/mobiledevelopmentfoodapp/MainViewModel.kt
@@ -1,12 +1,16 @@
 
 package com.mobiledevelopmentfoodapp
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mobiledevelopmentfoodapp.dto.Food
 import com.mobiledevelopmentfoodapp.service.RestaurantService
 import kotlinx.coroutines.launch
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 class MainViewModel : ViewModel() {
 
@@ -15,10 +19,25 @@ class MainViewModel : ViewModel() {
 
     private lateinit var firestore : FirebaseFirestore
 
+    init {
+        firestore = FirebaseFirestore.getInstance()
+        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+    }
+
     fun fetchRestaurants() {
         viewModelScope.launch {
             var innerRestaurant = RestaurantService.fetchFoods()
             restaurant.postValue(innerRestaurant)
+            var foodList = firestore.collection("FoodItems")
+                .get()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        Log.d(TAG, "DocumentSnapshot data: ${document.id} => ${document.data}")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents: ", exception)
+                }
         }
     }
 }
