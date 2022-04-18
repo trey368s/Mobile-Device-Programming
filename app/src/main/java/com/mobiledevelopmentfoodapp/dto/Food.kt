cba3bed22edd7722064b683c7d16266d6c294f30
Jubn-yan <jubn@mail.uc.edu>
2022-04-19 00:18:48 +0800

    add default value to food class

--- app/src/main/java/com/mobiledevelopmentfoodapp/dto/Food.kt
+++ app/src/main/java/com/mobiledevelopmentfoodapp/dto/Food.kt
@@ -3,11 +3,9 @@ package com.mobiledevelopmentfoodapp.dto
 //import androidx.room.Entity
 //import androidx.room.PrimaryKey
 
-data class Food(var name: String,
-                var description: String,
-                var price: Double,
+data class Food(var name: String?= null,
+                var description: String?=null,
+                var price: Double?=null,
                 @SerializedName("id") var productId:Int = 0 ) {
-    override fun toString(): String {
-        return name
-    }
+
 }
