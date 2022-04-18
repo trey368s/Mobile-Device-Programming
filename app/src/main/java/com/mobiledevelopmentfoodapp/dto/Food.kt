9ef0f49e9f5c492a9d4c2f3baa3e60258b1c1dcf
Jubn-yan <jubn@mail.uc.edu>
2022-04-19 00:06:07 +0800

    Revert "Revert "Merge pull request #20 from trey368s/caudilmr_codeReview1""
    This reverts commit bdfbf102d455f5e3414874082066e3f77b6a96f4.

--- app/src/main/java/com/mobiledevelopmentfoodapp/dto/Food.kt
+++ app/src/main/java/com/mobiledevelopmentfoodapp/dto/Food.kt
@@ -3,7 +3,10 @@ package com.mobiledevelopmentfoodapp.dto
 //import androidx.room.Entity
 //import androidx.room.PrimaryKey
 
-data class Food(var name: String, var description: String, var price: Int, var  Id:String) {
+data class Food(var name: String,
+                var description: String,
+                var price: Double,
+                @SerializedName("id") var productId:Int = 0 ) {
     override fun toString(): String {
         return name
     }
